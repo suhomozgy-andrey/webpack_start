661acb35740181193b0dd7eee3804477ec774107
Suhomozgy Andrey <suhomozgy.andrey@jetrocket.ru>
2017-01-19 23:34:24 +0300

    Some changes

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-
-const env = process.env.NODE_ENV || 'development';
-
-module.exports = merge(
-  require('./webpack_base.config.js'),
-  require(`./webpack_${env}.config.js`)
-);
